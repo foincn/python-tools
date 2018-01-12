ad3de04049992b30e970b472c15e7ee54e27c419
tz1006 <a@omg.tf>
2018-01-11 23:23:42 -0800

    Update r.py

--- r.py
+++ r.py
@@ -3,24 +3,32 @@
 # filename: r.py
 
 import requests
+from bs4 import BeautifulSoup
 
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14'
 ua_m = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1'
-header = {'User-Agent':ua_d}
+header = {'User-Agent':ua_m}
 s = requests.session()
-s.Keep-alive = False
+s.keep_alive = False
 
-print('''
------Desktop-----
-Default
------Mobile-----
-header = {\'User-Agent\':ua_m}
------Command-----
-url = 
 
-r = s.get(url, headers=header)
+def help():
+    print('''
+    -----Desktop-----
+    header = {\'User-Agent\':ua}
+    -----Mobile-----
+    Default
+    header = {\'User-Agent\':ua_m}
+    -----Command-----
+    url = 
+    r = s.get(url, headers=header)
+    -----BeautifulSoup-----
+    html = r.content
+    soup = BeautifulSoup(html, "html.parser")
+    ''')
 
-''')
+help()
 
 import code
 code.interact(banner = "", local = locals())
+
