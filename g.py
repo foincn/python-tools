6fb168628ead9b8cddf6e0599c9ae63010c55f17
tz1006 <a@omg.tf>
2018-01-11 23:28:37 -0800

    Update g.py

--- g.py
+++ g.py
@@ -11,7 +11,7 @@ header = {'User-Agent':ua_mo}
 
 gh = Ghost()
 
-se_mo = Session(gh, user_agent=ua_mo, wait_timeout=20, wait_callback=None, display=True, viewport_size=(375, 553), download_images=True)
+se = Session(gh, user_agent=ua_mo, wait_timeout=20, wait_callback=None, display=True, viewport_size=(375, 553), download_images=True)
 
 
 
